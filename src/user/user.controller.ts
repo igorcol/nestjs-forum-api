ccd4a9bf75646df9893be61dc981c96a6b87c036
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-14 21:58:13 -0300

    feat(userController): User CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Prisma, User } from '@prisma/client';
 
@@ -7,9 +7,27 @@ import { Prisma, User } from '@prisma/client';
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Post('signup')
+  @Post()
   async signupUser(@Body() userData: Prisma.UserCreateInput): Promise<User> {
     return this.userService.createUser(userData)
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<User | null> {
+    return this.userService.user({ id: +id })
+  }
+
+  @Patch(':id')
+  async updateUser(@Body() userData: Prisma.UserUpdateInput, @Param('id') id: string): Promise<User> {
+    return this.userService.updateUser({
+      where: { id: +id },
+      data: userData
+    })
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return this.userService.deleteUser({ id: +id })
   }
 
 }
